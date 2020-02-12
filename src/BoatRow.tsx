9062be0e02e97205b8bae3e1bfa234b75564eba2
GregPK <grzegorz.kaczorek@gmail.com>
2020-02-12 22:28:40 +0100

    Implement navigation from index to show

--- src/BoatRow.tsx
+++ src/BoatRow.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-
-// import Link from "react-router-dom";
+import { navigate } from "hookrouter";
 
 class BoatRow extends React.Component<any> {
+  show = (event: any) => {
+    navigate("boats/" + this.props.boat.id)
+  }
+
   render() {
     return (
-      <tr className="App" >
+      <tr className="App" onClick={this.show}>
         <td>{this.props.boat.id}</td>
         <td>{this.props.boat.name}</td>
         <td>{this.props.boat.description}</td>
-      </tr>
+      </tr >
     )
   }
 }
