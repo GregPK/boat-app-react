6a2b4cae9357edcb0649a15127067d82cf2e7dd7
GregPK <grzegorz.kaczorek@gmail.com>
2020-02-14 17:19:42 +0100

    Add boat delete possibility

--- src/BoatRow.tsx
+++ src/BoatRow.tsx
@@ -6,11 +6,36 @@ class BoatRow extends React.Component<any> {
     navigate("boats/" + this.props.boat.id)
   }
 
+  delete = () => {
+    if (window.confirm('Are you sure you wish to delete this item?')) {
+      this.props.onDelete(this.props.boat.id)
+    }
+  }
+
   render() {
     return (
-      <tr className="App" onClick={this.show}>
+      <tr className="App">
         <td>{this.props.boat.id}</td>
-        <td>{this.props.boat.name}</td>
+        <td>
+          <div className="buttons">
+            <a href={"/boats/" + this.props.boat.id} className="button is-info is-small">
+              <span className="icon">
+                <i className="fas fa-eye"></i>
+              </span>
+            </a>
+            <a href={"/boats/" + this.props.boat.id + "/edit"} className="button is-warning is-small">
+              <span className="icon">
+                <i className="fas fa-edit"></i>
+              </span>
+            </a>
+            <button onClick={this.delete} className="button is-danger is-small">
+              <span className="icon">
+                <i className="fas fa-trash"></i>
+              </span>
+            </button>
+          </div>
+        </td>
+        <td onClick={this.show} className="name">{this.props.boat.name}</td>
         <td>{this.props.boat.description}</td>
       </tr >
     )
--- src/BoatTable.tsx
+++ src/BoatTable.tsx
@@ -17,13 +17,29 @@ class BoatTable extends React.Component<any> {
         }
       )
   }
+
+  onDeleteBoat = (boatId: string) => {
+    fetch("http://localhost:3004/boats/" + boatId, { method: 'DELETE' })
+      .then(
+        (result) => {
+          this.setState({
+            boats: this.state.boats.filter((boat: any) => boat.id !== boatId)
+          })
+        },
+        (error) => {
+          console.error(error)
+        }
+      )
+  }
+
   render() {
-    let boatComps = this.state.boats.map((boat: any) => <BoatRow key={boat.id} boat={boat} />)
+    let boatComps = this.state.boats.map((boat: any) => <BoatRow key={boat.id} boat={boat} onDelete={this.onDeleteBoat} />)
     return (
       <table className="boat-table table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
         <thead>
           <tr>
             <td>Id</td>
+            <td>Action</td>
             <td>Name</td>
             <td>Description</td>
           </tr>
